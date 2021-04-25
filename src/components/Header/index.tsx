35f65bb804bcf0691e915c15bf91a259ce1777b6
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-04-25 11:31:00 -0300

    Inclusão de darkTheme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,12 @@ import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
-export function Header() {
+export function Header({ toggleTheme, theme }) {
 
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
     locale: ptBR
   });
+
   return (
     <header className={styles.headerContainer}>
       <Link href="/">
@@ -19,6 +20,10 @@ export function Header() {
 
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
+      <button type="button" onClick={() => toggleTheme()} className={styles.themeButton}>
+        <img src={theme ? '/lightbulb_outline.svg' : '/lightbulb.svg'} alt={theme ? 'Ir para modo claro' : 'Ir para modo escuro'} />
+        <span>{theme ? 'Ir para modo claro' : 'Ir para modo escuro'}</span>
+      </button>
     </header>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,20 @@ import styles from '../styles/app.module.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import { PlayerContextProvider } from '../contexts/PlayerContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function toggleTheme() {
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
     <PlayerContextProvider>
-      <div className={styles.wrapper} >
+      <div className={styles.wrapper + (isDarkMode ? ' dark' : ' light')} >
         <main>
-          <Header />
+          <Header toggleTheme={toggleTheme} theme={isDarkMode} />
           <Component {...pageProps} />
         </main>
         <Player />
